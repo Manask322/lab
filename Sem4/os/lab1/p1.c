a57126423a8f925cb8f46c6d1f1a0a6badab1a2e
shreyas <shreyasseshadri@gmail.com>
2018-01-13 10:54:09 +0530

    OS assgn1

--- Sem4/os/lab1/p1.c
+++ Sem4/os/lab1/p1.c
@@ -1,48 +1,133 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node 
+struct block
 {
-  int data;
-  struct node* next;
+   int size;
+   int is_free;
 };
-struct node* newNode(int key)
+void  best_allocate(struct block blocks[],int m,int process[],int n)
 {
-	struct node* temp=(struct node*)malloc(sizeof(struct node));
-	temp->data=key;temp->next=NULL;
+  int al_process[n];
+  int i,j;
+  for(i=0;i<n;i++)al_process[i]=-1;
+  for(i=0;i<n;i++)
+  {
+    int min_j=-1;
+    for(j=0;j<m;j++)
+    {
+      if(blocks[j].size>=process[i])
+      {
+        if(min_j==-1)min_j=j;
+        else if(blocks[min_j].size>blocks[j].size)min_j=j;
+      }
+    }
+    if(min_j!=-1)
+    {
+      al_process[i]=min_j;
+      blocks[min_j].size-=process[i];
+      blocks[min_j].is_free=0;
+    }
+  }
+  for(i=0;i<n;i++)
+  { if(al_process[i]!=-1)
+    printf("Process no.:%d  Process size:%d  Block:%d Free memory in block:%d \n",i+1,process[i],al_process[i]+1,blocks[al_process[i]].size);
+    else
+    printf("Process no.:%d  Process size:%d  Block:Not allocated \n",i+1,process[i]);
+  }
 }
-void main()
+void first_allocate(struct block blocks[],int m,int process[],int n)
 {
-	// int *a;
-	// // int b;
-	// a=(int*)malloc(44*sizeof(int));
-	// printf("%p to %p",a,a+sizeof(a));
-	// // printf("%ld",sizeof(a));
-	// free(a);
- //    #define K (1024)
- //         char *p1,*p2;
- //         p1 = (char *)malloc(3*K);
- //          p2 = (char *)malloc(4*K);
- //         free(p1);
- //          p1 = (char *)malloc(4*K);
- //         if(p1==NULL)printf("Null alloc");  
-	struct node* node1=newNode(0);
-	struct node* node2=newNode(1);
-	struct node* node3=newNode(2);
-	struct node* node4=newNode(3);
-	struct node* node5=newNode(4);
- //    printf("%d \n",node1);
- //    printf("%ld \n",sizeof(node1));
- //    printf("%d \n",node2);
- //    printf("%ld",sizeof(node2));
-    struct node* block[6]; 
-     block[0]=node1;
-     block[1]=node2;
-     block[2]=node3;
-     int i;
-     // for(i=0;i<=2;i++)
-     // 	// printf("%d\n",block[i]);
-     free(node1);
-     printf("%d\n", node1);
-     // for(i=0;i<=2;i++)
-     // 	// printf("%d\n",block[i]);
+  int al_process[n];
+  int i,j;
+  for(i=0;i<n;i++)al_process[i]=-1;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<m;j++)
+    {
+      if(blocks[j].size>=process[i])
+      {
+        al_process[i]=j;
+        blocks[j].size-=process[i];
+        blocks[j].is_free=0;
+        break;
+      }
+    }
+  }
+  for(i=0;i<n;i++)
+  { if(al_process[i]!=-1)
+    printf("Process no.:%d  Process size:%d  Block:%d Free memory in block:%d \n",i+1,process[i],al_process[i]+1,blocks[al_process[i]].size);
+    else
+    printf("Process no.:%d  Process size:%d  Block:Not allocated \n",i+1,process[i]);
+  }
+}
+void worst_allocate(struct block blocks[],int m,int process[],int n)
+{
+   int al_process[n];
+   int i,j;
+   for(i=0;i<n;i++)al_process[i]=-1;
+   for(i=0;i<n;i++)
+   {
+     int worst_j=-1;
+     for(j=0;j<m;j++)
+     {
+       if(blocks[j].size>=process[i])
+       {
+         if(worst_j==-1)worst_j=j;
+         else if(blocks[worst_j].size>=process[i])worst_j=j;
+       }
+     }
+     if (worst_j != -1)
+        {
+            al_process[i] = worst_j;
+            blocks[worst_j].size -= process[i];
+            blocks[worst_j].is_free=0;
+        }
+   }
+   for(i=0;i<n;i++)
+  { if(al_process[i]!=-1)
+    printf("Process no.:%d  Process size:%d  Block:%d Free memory in block:%d \n",i+1,process[i],al_process[i]+1,blocks[al_process[i]].size);
+    else
+    printf("Process no.:%d  Process size:%d  Block:Not allocated \n",i+1,process[i]);
+  }
+}
+int main()
+{
+  // int blockSize[] = {100, 500, 200, 300, 600};
+  //   int processSize[] = {212, 417, 112, 426};
+  int n;
+  printf("How many partitions\n");
+  scanf("%d",&n);
+  int i;
+  struct block blocks[n];
+  printf("Enter sizes\n");
+  for(i=0;i<n;i++)
+  { blocks[i].is_free=1;
+    scanf("%d",&blocks[i].size);
+  }
+  printf("How many process you want\n");
+  int m;
+  scanf("%d",&m);
+  int process[m];
+  printf("Enter process\n");
+  for(i=0;i<m;i++)
+  {
+  scanf("%d",&process[i]);  
+  } 
+  printf("Which fit?\n1.Best\n2.First\n3.worst\n");
+  int o;
+  scanf("%d",&o);
+  switch(o)
+  {
+   case 1: best_allocate(blocks,n,process,m);
+          break;
+   case 2: first_allocate(blocks,n,process,m);
+           break;  
+    case 3:worst_allocate(blocks,n,process,m);
+           break;  
+  //  case 4:best_allocate(blocks,n,process,m);
+  //         printf("********************\n");
+  //         first_allocate(blocks,n,process,m);
+  //         printf("********************\n");
+  //         worst_allocate(blocks,n,process,m);
+  }
 }
