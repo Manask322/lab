62b7d2e1a67d60b05f7f81a8fbe35aedfb3106d3
shreyas <shreyasseshadri@gmail.com>
2018-02-18 12:45:33 +0530

    fixed peak element

--- Sem4/daa/lab2/p2.c
+++ Sem4/daa/lab2/p2.c
@@ -2,13 +2,14 @@
 #include<stdlib.h>
 // int peak(int a[],int low,int high);
 int peak(int *a,int low,int high)
-{
+{   if(low==high)return a[low];
 	if(low<=high)
 	{
 		int mid=(low+high)/2;
+		if(mid-1<0)return a[high];
 		if(a[mid]>a[mid-1])return peak(a,mid,high);
 		else
-		{
+		{   
 			if(a[mid-1]>a[mid])return peak(a,low,mid-1);
 			else return a[mid];
 		}
@@ -20,6 +21,6 @@ void  main()
 	int a[]={2,4,6,8,7,5,3};
 	int b[]={10, 12, 8, 4 , -3, -15};
 	int a1=peak(a,0,6);
-	printf("%d",a1);
-	// printf("%d",peak(b,0,5));
+	printf("%d\n",a1);
+	printf("%d",peak(b,0,5));
 }
