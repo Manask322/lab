cc00b4cacda99a64d9b29e1a4e8437a696e95174
shreyas <shreyasseshadri@gmail.com>
2018-09-02 17:47:06 +0530

    MLP

--- Sem5/Soft Computing/lab2/MLP.py
+++ Sem5/Soft Computing/lab2/MLP.py
@@ -17,10 +17,14 @@ def layer_sizes(X, Y):
 
 def initialize_parameters(n_x, n_h, n_y):
     np.random.seed(2)
-    W1 = np.random.randn(n_h,n_x)*0.01
-    b1 = np.zeros((n_h,1))
-    W2 = np.random.randn(n_y,n_h)*0.01
-    b2 = np.zeros((n_y,1))
+    # W1 = np.random.randn(n_h,n_x)*0.01
+    # b1 = np.zeros((n_h,1))
+    # W2 = np.random.randn(n_y,n_h)*0.01
+    # b2 = np.zeros((n_y,1))
+    W1=np.full((n_h,n_x),1/(n_x+1))
+    b1=np.full((n_h,1),1/(n_x+1))
+    W2=np.full((n_y,n_h),1/(n_h+1))
+    b2=np.full((n_y,1),1/(n_h+1))
     assert (W1.shape == (n_h, n_x))
     assert (b1.shape == (n_h, 1))
     assert (W2.shape == (n_y, n_h))
@@ -106,9 +110,8 @@ def update_parameters(parameters, grads, learning_rate = 1.2):
     
     return parameters
 
-def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=True):
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
     np.random.seed(3)
-    # print(X.shape)
     n_x = layer_sizes(X, Y)[0]
     n_y = layer_sizes(X, Y)[2]
  
@@ -132,25 +135,23 @@ def predict(parameters, X):
     predictions=[1 if A2[0][i]>0.5 else 0   for i in range(A2.shape[1])] 
     return predictions
 
-data=pd.read_csv("../datasets/SPECT.csv",delimiter=",")
-
-data = data.sample(frac=1,random_state=123).reset_index(drop=True)
-
-X=data.drop(['Class'],axis=1)
-y=data['Class'].replace(['Yes','No'],[1,0])
-m=X.shape[0]
-test_size=int(0.7*m)
-X_train=X[:test_size]
-y_train=y[:test_size]
-X_test=X[test_size:]
-y_test=y[test_size:]
-X_train=np.array(X_train).T
-y_train=np.array(y_train).reshape(1,y_train.shape[0])
-X_test=np.array(X_test).T
-y_test=np.array(y_test).reshape(1,y_test.shape[0])
-
-parameters=nn_model(X_train,y_train,5)
-pred=predict(parameters,X_test)
+# def preprocess_data(file,label):
+#     data=pd.read_csv("../datasets/"+file,delimiter=",")
+#     data = data.sample(frac=1,random_state=123).reset_index(drop=True)
+#     data[label] = pd.Categorical.from_array(data[label]).labels
+#     X=data.drop([label],axis=1)
+#     y=data[label]
+#     m=X.shape[0]
+#     test_size=int(0.7*m)
+#     X_train=X[:test_size]
+#     y_train=y[:test_size]
+#     X_test=X[test_size:]
+#     y_test=y[test_size:]
+#     X_train=np.array(X_train).T
+#     y_train=np.array(y_train).reshape(1,y_train.shape[0])
+#     X_test=np.array(X_test).T
+#     y_test=np.array(y_test).reshape(1,y_test.shape[0])
+#     return X_train,y_train,X_test,y_test
 
 def accuracy_metric(actual, predicted):
 	correct = 0
@@ -160,25 +161,136 @@ def accuracy_metric(actual, predicted):
 	return correct / float(len(actual)) * 100.0
 
 def confusion_matrix(y,y_pred):
-	tp=0
-	fp=0
-	fn=0
-	tn=0
-	for i,j in zip(y,y_pred):
-		if i==0:
-			if j==0:
-				tn+=1
-			else:
-				fp+=1
-		elif i==1:
-			if j==1:
-				tp+=1
-			else:
-				fn+=1
-	precision=tp/(tp+fp)
-	recall=tp/(tp+fn)
-	return precision,recall
+    tp=0
+    fp=0
+    fn=0
+    tn=0
+    precision=None
+    recall=None
+    for i,j in zip(y,y_pred):
+        if i==0:
+            if j==0:
+                tn+=1
+            else:
+                fp+=1
+        elif i==1:
+            if j==1:
+                tp+=1
+            else:
+                fn+=1
+    try:
+        precision=tp/(tp+fp)
+        recall=tp/(tp+fn)
+    except:
+        print("zero division")
+    return precision,recall
 
-# print(list(pred))
-print(accuracy_metric(y_test.reshape(-1,1),pred))
-print(confusion_matrix(y_test.reshape(-1,1),pred))
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for _ in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
+
+def evaluate_algorithm(dataset, n_folds):
+        folds = cross_validation_split(dataset, n_folds)
+        scores = list()
+        f=1
+        for fold in folds:
+            train_set = list(folds)
+            train_set.remove(fold)
+            train_set = sum(train_set, [])
+            test_set = list()
+            actual=[]
+            for row in fold:
+                row_copy = list(row)
+                test_set.append(row_copy)
+                actual.append(row[-1])
+            train=np.array(train_set)
+            X_train=train[:,:train.shape[1]-1].T
+            y_train=train[:,-1].reshape(1,train.shape[0])
+            test=np.array(test_set)
+            X_test=test[:,:test.shape[1]-1].T
+            actual=test[:,-1].reshape(1,test.shape[0])
+            parameters=nn_model(X_train,y_train,5)
+            predicted = predict(parameters,X_test)
+            print("FOLD ",f)
+            print("predicted :",predicted)
+            print("actual :",actual)
+            accuracy = accuracy_metric(actual.reshape(-1,1), predicted)
+            precision,recall=confusion_matrix(actual.reshape(-1,1),predicted)
+            print("accuracy :"+str(accuracy))
+            print("precision :"+str(precision))
+            print("recall :"+str(recall))
+            print("-"*90)
+            scores.append(["accuracy :"+str(accuracy),"precision :"+str(precision),"recall :"+str(recall)])
+            f+=1
+
+def get_data(file,y_in):
+	reader = csv.reader(open("../datasets/"+file),delimiter=",")
+	data=[]
+	c=0
+	for row in reader:
+		if(c==0):
+			c+=1
+			continue
+		data.append(row)
+	random.seed(123)
+	shuffle(data)
+	for item in data:
+		item[-1], item[y_in] = item[y_in], item[-1]
+	
+	X=[]
+	y=[]
+	c=0
+	
+	for row in data:
+		X.append(row[:-1])
+		y.append(row[-1])
+	unique=[]
+	for i in range(len(y)):
+		if y[i] not in unique:
+			unique.append(y[i])
+	for i in range(len(y)):
+		y[i]=unique.index(y[i])
+	for i in range(len(X)):
+		for j in range(len(X[0])):
+			X[i][j]=float(X[i][j])
+	for i in range(len(X)):
+		X[i]=[1]+X[i]
+	data=[]
+	for i in range(len(X)):
+		data.append(X[i]+[y[i]])
+	return data
+
+
+iris=get_data("IRIS.csv",-1)
+print("For IRIS dataset")
+evaluate_algorithm(iris,10)
+print("="*100)
+print("For SPECT dataset")
+scept=get_data("SPECT.csv",0)
+evaluate_algorithm(scept,10)
+
+
+# X_train,y_train,X_test,y_test=preprocess_data("SPECT.csv",'Class')
+# parameters=nn_model(X_train,y_train,5)
+# pred=predict(parameters,X_test)
+
+# print("SPECT dataset:")
+# print("accuracy :"+accuracy_metric(y_test.reshape(-1,1),pred))
+# print(" (precision,recall): "+confusion_matrix(y_test.reshape(-1,1),pred))
+
+# X_train,y_train,X_test,y_test=preprocess_data("IRIS.csv",'class')
+# parameters=nn_model(X_train,y_train,5)
+# pred=predict(parameters,X_test)
+
+# print("IRIS dataset:")
+# print("accuracy :"+accuracy_metric(y_test.reshape(-1,1),pred))
+# print(" (precision,recall): "+confusion_matrix(y_test.reshape(-1,1),pred))
+
